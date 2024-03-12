5a2f9c292fdd429eb4b20ce179a41e95883b67ef
Rounit Ranjan Sinha <rounitrsinha@gmail.com>
2024-03-12 14:50:23 +0530

    feat-added react-flow-form package

---
+++ index.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function Multistepform(steps = []) {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  function next() {
+    setCurrentStepIndex((i) => {
+      if (i >= steps.length - 1) return i;
+      return i + 1;
+    });
+  }
+
+  function back() {
+    setCurrentStepIndex((i) => {
+      if (i <= 0) return i;
+      return i - 1;
+    });
+  }
+
+  function goTo(index) {
+    setCurrentStepIndex(index);
+  }
+
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    goTo,
+    isFirstStep: currentStepIndex === 0,
+    isLastStep: currentStepIndex === steps.length - 1,
+    next,
+    back,
+    steps,
+  };
+}
+
+export default Multistepform;
